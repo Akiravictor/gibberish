4f562900356ac4c8b434333383af037772cbb1e9
Victor Akira Hassuda Silva <victorakira@hotmail.com>
2021-07-14 00:13:17 -0300

    aaa

---
+++ Algoritmos/Dominio/DataStructures/BinaryTree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.DataStructures
+{
+	public class BinaryTree
+	{
+		public int Id { get; set; }
+
+		public BinaryTree Left { get; set; }
+
+		public BinaryTree Right { get; set; }
+
+
+		public BinaryTree(int value)
+		{
+			this.Id = value;
+		}
+
+		public BinaryTree()
+		{
+		}
+
+		public void Insert(int value)
+		{
+
+		}
+
+		public void Remove(int value)
+		{
+
+		}
+
+		public void Balance()
+		{
+
+		}
+	}
+}
